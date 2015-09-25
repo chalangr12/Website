7860b9c32ffd76efaa34c931a49a6a719dcc34d5
unknown <578612@BAHCND5052R6R.resource.ds.bah.com>
2015-09-25 15:06:58 -0400

    finished fixing up fuck up

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var serveStatic = require('serve-static');
  
 var app = express();
  
-<<<<<<< HEAD
 app.use(serveStatic('./public', {'index': ['index.html', 'index.html']}));
-=======
-app.use(serveStatic('./', {'index': ['index.html', 'index.htm']}));
 console.log('App listening at port 3000');
->>>>>>> bd7d4f51acef2a4103561ecd024839468ac47f95
 app.listen(3000);
