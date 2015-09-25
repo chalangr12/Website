5c1173f1e94158e31fc74771471a2159f79af053
unknown <578612@BAHCND5052R6R.resource.ds.bah.com>
2015-09-25 12:02:48 -0400

    Fixed nodemailer

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var serveStatic = require('serve-static');
  
 var app = express();
  
-app.use(serveStatic('./public', {'index': ['index.html', 'index.htm']}));
+app.use(serveStatic('./public', {'index': ['index.html', 'index.html']}));
 app.listen(3000);
